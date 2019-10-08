9de5e2b9e4b4ee3211e450575484a25a7ccebeb9
koenigmm <marius.koenig@outlook.com>
2019-10-08 21:07:19 +0200

    helloWorld console and html

---
+++ L01 HelloWorld/test.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("Hello World");
+window.addEventListener("load", handLoad);
+let t = "Hello World";
+function handLoad(event) {
+    document.body.innerHTML = "Hello World";
+}
+//# sourceMappingURL=test.js.map
---
+++ L01 HelloWorld/test.ts
@@ -0,0 +1,6 @@
+console.log("Hello World");
+window.addEventListener("load", handLoad);
+let t: string = "Hello World";
+function handLoad(event : Event): void {
+    document.body.innerHTML = "Hello World";
+}
