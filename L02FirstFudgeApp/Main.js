73ba1133bb45413e457bd298b7806d6659cf7427
koenigmm <marius.koenig@outlook.com>
2019-10-17 15:06:24 +0200

    zwei meshes im Viewport

--- L02FirstFudgeApp/Main.js
+++ L02FirstFudgeApp/Main.js
@@ -9,14 +9,28 @@ var L02FirstFudge;
         console.log(canvas);
         let node = new fudge.Node("Quad");
         let mesh = new fudge.MeshQuad();
+        //let mesh: fudge.MeshCube = new fudge.MeshCube();
         let cmpMesh = new fudge.ComponentMesh(mesh);
         node.addComponent(cmpMesh);
-        let mtrSolidWhite = new fudge.Material("Solid White", fudge.ShaderUniColor, new fudge.CoatColored(new fudge.Color(1, 1, 1, 1)));
+        cmpMesh.pivot.translateZ(0.1);
+        cmpMesh.pivot.rotateZ(4);
+        cmpMesh.pivot.translateX(0.2);
+        let mtrSolidWhite = new fudge.Material("Solid White", fudge.ShaderUniColor, new fudge.CoatColored(new fudge.Color(0.2, 1, 1, 1)));
         let mtrComponent = new fudge.ComponentMaterial(mtrSolidWhite);
         node.addComponent(mtrComponent);
         let camera = new fudge.ComponentCamera();
-        camera.pivot.translateZ(2);
-        //camera.pivot.rotateX(5);
+        camera.pivot.translateZ(3);
+        camera.pivot.rotateX(5);
+        camera.pivot.rotateY(12);
+        let node02 = new fudge.Node("Cube");
+        let meshcube = new fudge.MeshCube();
+        let mtrGreen = new fudge.Material("Green", fudge.ShaderUniColor, new fudge.CoatColored(new fudge.Color(0, 0.9, 0, 0)));
+        let mtrCubeMaterial = new fudge.ComponentMaterial(mtrGreen);
+        let cmpCube = new fudge.ComponentMesh(meshcube);
+        node02.addComponent(cmpCube);
+        node02.addComponent(mtrCubeMaterial);
+        cmpCube.pivot.translateY(1);
+        node.appendChild(node02);
         let viewport = new fudge.Viewport();
         viewport.initialize("Viewport", node, camera, canvas);
         fudge.Debug.log(viewport);
@@ -24,9 +38,15 @@ var L02FirstFudge;
         //fudge.Matrix4x4() Translate ausprobieren eigentlich schon gemacht. 
     }
 })(L02FirstFudge || (L02FirstFudge = {}));
+//TODO
 //Node Modules 
 //Package Json mit ins Verzeichnis nehmen 
 // Gitignore anlegen damit sie nicht von Git mitgenommen werden
-// IN Canvas weißes Rechteck erzeugen
 // Kamera standardmäßig im Weltursprung, deshalb muss die Kamera in z-Richtung bewegt werden.
+/*
+Bis Donnerstag weiter mit den Systemen beschäftigen
+Translate, Pivot, Matrizen, Meshes usw.
+Look at verwenden
+ausprobieren
+*/
 //# sourceMappingURL=Main.js.map
--- L02FirstFudgeApp/Main.ts
+++ L02FirstFudgeApp/Main.ts
@@ -8,15 +8,32 @@ namespace L02FirstFudge {
 
         let node: fudge.Node = new fudge.Node("Quad");
         let mesh: fudge.MeshQuad = new fudge.MeshQuad();
+        //let mesh: fudge.MeshCube = new fudge.MeshCube();
         let cmpMesh: fudge.ComponentMesh = new fudge.ComponentMesh(mesh);
         node.addComponent(cmpMesh);
+        cmpMesh.pivot.translateZ(0.1);
+        cmpMesh.pivot.rotateZ(4);
+        cmpMesh.pivot.translateX(0.2);
 
-        let mtrSolidWhite: fudge.Material = new fudge.Material("Solid White", fudge.ShaderUniColor, new fudge.CoatColored(new fudge.Color(1, 1, 1, 1)));
+        let mtrSolidWhite: fudge.Material = new fudge.Material("Solid White", fudge.ShaderUniColor, new fudge.CoatColored(new fudge.Color(0.2, 1, 1, 1)));
         let mtrComponent: fudge.ComponentMaterial = new fudge.ComponentMaterial(mtrSolidWhite);
         node.addComponent(mtrComponent);
         let camera: fudge.ComponentCamera = new fudge.ComponentCamera();
-        camera.pivot.translateZ(2);
-        //camera.pivot.rotateX(5);
+        camera.pivot.translateZ(3);
+        camera.pivot.rotateX(5);
+        camera.pivot.rotateY(12);
+
+        let node02: fudge.Node = new fudge.Node("Cube");
+        let meshcube: fudge.MeshCube = new fudge.MeshCube();
+        let mtrGreen: fudge.Material = new fudge.Material("Green", fudge.ShaderUniColor, new fudge.CoatColored(new fudge.Color(0, 0.9, 0, 0)));
+        let mtrCubeMaterial: fudge.ComponentMaterial = new fudge.ComponentMaterial(mtrGreen);
+        let cmpCube: fudge.ComponentMesh = new fudge.ComponentMesh(meshcube);
+        node02.addComponent(cmpCube);
+        node02.addComponent(mtrCubeMaterial);
+        cmpCube.pivot.translateY(1);
+
+        node.appendChild(node02);
+        
         let viewport: fudge.Viewport = new fudge.Viewport();
         viewport.initialize("Viewport", node, camera, canvas);
         fudge.Debug.log(viewport);
