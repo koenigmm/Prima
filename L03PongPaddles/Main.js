911e6c4c617d6635ffddceb161bb66e8e65e7c7b
Marius König <marius.koenig@outlook.com>
2019-10-29 14:08:59 +0100

    funktionierende update funktion

--- L03PongPaddles/Main.js
+++ L03PongPaddles/Main.js
@@ -3,6 +3,18 @@ var L03PongPaddles;
 (function (L03PongPaddles) {
     var fudge = FudgeCore;
     window.addEventListener("load", hndload);
+    let keysPressed = {};
+    let paddleLeft;
+    let paddleRight;
+    let viewport = new fudge.Viewport();
+    //Fude Keyboad Codes verwenden
+    function hndlKeyDown(_event) {
+        keysPressed[_event.key] = true;
+    }
+    //vielleicht besser mit fudge codes arbeiten
+    function hndlKeyUp(_event) {
+        keysPressed[_event.key] = false;
+    }
     function hndload(_event) {
         const canvas = document.querySelector("canvas");
         fudge.RenderManager.initialize();
@@ -10,40 +22,21 @@ var L03PongPaddles;
         let pong = createPong();
         let camera = new fudge.ComponentCamera();
         camera.pivot.translateZ(3);
-        let viewport = new fudge.Viewport();
         viewport.initialize("Viewport", pong, camera, canvas);
         fudge.Debug.log(viewport);
         viewport.draw();
-        addEventListener("keydown", event => {
-            let childenListLeft = pong.getChildrenByName("LeftPaddle");
-            let childenListRight = pong.getChildrenByName("RightPaddle");
-            let paddleLeft = childenListLeft[0];
-            let paddleRight = childenListRight[0];
-            switch (event.key) {
-                case "w":
-                    paddleRight.cmpTransform.local.translateY(0.1);
-                    break;
-                case "s":
-                    paddleRight.cmpTransform.local.translateY(-0.1);
-                    break;
-                case "ArrowUp":
-                    paddleLeft.cmpTransform.local.translateY(0.1);
-                    break;
-                case "ArrowDown":
-                    paddleLeft.cmpTransform.local.translateY(-0.1);
-                    break;
-            }
-            fudge.RenderManager.update();
-            viewport.draw();
-        });
+        addEventListener("keydown", hndlKeyDown);
+        addEventListener("keyup", hndlKeyUp);
         viewport.draw();
+        fudge.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        fudge.Loop.start();
     }
     function createPong() {
         //Nodes
         let pongNode = new fudge.Node("Pong Node");
         let ball = new fudge.Node("Ball");
-        let leftPaddle = new fudge.Node("LeftPaddle");
-        let rightPaddle = new fudge.Node("RightPaddle");
+        paddleLeft = new fudge.Node("paddleLeft");
+        paddleRight = new fudge.Node("paddleRight");
         // Left Paddle Compnent und Mesh und Color
         let meshQuad = new fudge.MeshQuad();
         let cmpMesh = new fudge.ComponentMesh(meshQuad);
@@ -58,30 +51,47 @@ var L03PongPaddles;
         let cmpBall = new fudge.ComponentMesh(meshBall);
         let BallMtrComponent = new fudge.ComponentMaterial(mtrSolidWhite);
         //Left Paddle Node zusamensetzten und zu Pong Node hinzufügen
-        leftPaddle.addComponent(mtrComponent);
-        leftPaddle.addComponent(cmpMesh);
-        leftPaddle.addComponent(new fudge.ComponentTransform);
-        pongNode.appendChild(leftPaddle);
+        paddleLeft.addComponent(mtrComponent);
+        paddleLeft.addComponent(cmpMesh);
+        paddleLeft.addComponent(new fudge.ComponentTransform);
+        pongNode.appendChild(paddleLeft);
         //Right Paddle Node zusammensetzten und zu Pong Node hinzufügen
-        rightPaddle.addComponent(quad02MtrComponent);
-        rightPaddle.addComponent(cmpQuad02);
-        rightPaddle.addComponent(new fudge.ComponentTransform);
-        pongNode.appendChild(rightPaddle);
+        paddleRight.addComponent(quad02MtrComponent);
+        paddleRight.addComponent(cmpQuad02);
+        paddleRight.addComponent(new fudge.ComponentTransform);
+        pongNode.appendChild(paddleRight);
         //Ball Node zusammensetzten und zu Pong Node hinzufügen
         ball.addComponent(cmpBall);
         ball.addComponent(BallMtrComponent);
         ball.addComponent(new fudge.ComponentTransform);
         pongNode.appendChild(ball);
-        //let cmpTransform: fudge.ComponentTransform = rightPaddle.getComponent(fudge.ComponentTransform) //cmpTransform geht auch
-        leftPaddle.cmpTransform.local.translateX(1.3);
-        rightPaddle.cmpTransform.local.translateX(-1.3);
+        //let cmpTransform: fudge.ComponentTransform = paddleRight.getComponent(fudge.ComponentTransform) //cmpTransform geht auch
+        paddleLeft.cmpTransform.local.translateX(1.3);
+        paddleRight.cmpTransform.local.translateX(-1.3);
         // TODO Nebeneffekte möglich Besser das Mesh Skalieren
         ball.cmpTransform.local.scaleY(0.2);
         ball.cmpTransform.local.scaleX(0.2);
         ball.cmpTransform.local.scaleZ(0.2);
-        leftPaddle.cmpTransform.local.scaleX(0.15);
-        rightPaddle.cmpTransform.local.scaleX(0.15);
+        paddleLeft.cmpTransform.local.scaleX(0.15);
+        paddleRight.cmpTransform.local.scaleX(0.15);
         return pongNode;
+    }
+    function update(_event) {
+        //fudge.Debug.log(keysPressed);
+        fudge.RenderManager.update();
+        viewport.draw();
+        if (keysPressed["w"]) {
+            paddleRight.cmpTransform.local.translateY(0.025);
+        }
+        if (keysPressed["s"]) {
+            paddleRight.cmpTransform.local.translateY(-0.025);
+        }
+        if (keysPressed["ArrowUp"]) {
+            paddleLeft.cmpTransform.local.translateY(0.025);
+        }
+        if (keysPressed["ArrowDown"]) {
+            paddleLeft.cmpTransform.local.translateY(-0.025);
+        }
     }
 })(L03PongPaddles || (L03PongPaddles = {}));
 /*
@@ -91,10 +101,20 @@ Objekte müssen bewegt werden
 Vorischt was man bewegen will. Die Meshes oder vielleicht doch besser die Knoten
 Backface Culling beachten
 set invervall und alle x Sekunden etwas aufrufen.
-Vorsicht mit anonymen Funktionen
+Vorsicht mit anonymen Funktionen => innerhalb von eventlistener
 
-Hausaufgaben
-Paddles mit Tasen rauf und runter bewgen.
-Knoten Transformation benuten
+Tastatureingabe
+Möglichkeit Nummer 1 Array String speichern und bei Veränderung reagieren
+Möglichkeit Nummer 2 Assoziatives Array (Map oder standart Javascript Objekte, also geschweifte Klammern)
+let keyspressed = {} Assoziatives Array
+Keydown: Wenn was kommt in das Array den Code speichern der ankomme
+Keyup: Den Code auf false setzten oder löschen
+Herausfinden, ob ein bestimmter Key gedrückt wurde
+    File Keypressed fudge keyboard code bspw. up ist true, dann darauf reagieren
+
+Aufgabe für Dienstag
+Ball bewegen, so dass er an allen vier Wänden abprall. Starten in zufällige Richtung und Geschwindigkeit
+Vector3 verwenden für die Geschwindigkeit soll am Anfang zufällig bestimmt werden
+Ränder definieren
 */ 
 //# sourceMappingURL=Main.js.map
--- L03PongPaddles/Main.ts
+++ L03PongPaddles/Main.ts
@@ -1,56 +1,52 @@
 namespace L03PongPaddles {
-    import fudge = FudgeCore;
 
+    interface KeysPressed {
+        [code: string]: boolean
+    }
+
+    import fudge = FudgeCore;
     window.addEventListener("load", hndload);
+    let keysPressed: KeysPressed = {};
+    let paddleLeft: fudge.Node;
+    let paddleRight: fudge.Node;
+    let viewport: fudge.Viewport = new fudge.Viewport();
+
+
+
+    //Fude Keyboad Codes verwenden
+    function hndlKeyDown(_event: KeyboardEvent): void {
+        keysPressed[_event.key] = true;
+    }
+
+    //vielleicht besser mit fudge codes arbeiten
+    function hndlKeyUp(_event: KeyboardEvent): void {
+        keysPressed[_event.key] = false;
+    }
+
     function hndload(_event: Event): void {
         const canvas: HTMLCanvasElement = document.querySelector("canvas");
         fudge.RenderManager.initialize();
         console.log(canvas);
         let pong: fudge.Node = createPong();
-
         let camera: fudge.ComponentCamera = new fudge.ComponentCamera();
         camera.pivot.translateZ(3);
 
-        let viewport: fudge.Viewport = new fudge.Viewport();
         viewport.initialize("Viewport", pong, camera, canvas);
         fudge.Debug.log(viewport);
         viewport.draw();
-
-        addEventListener("keydown", event => {
-            let childenListLeft: fudge.Node[] = pong.getChildrenByName("LeftPaddle");
-            let childenListRight: fudge.Node[] = pong.getChildrenByName("RightPaddle");
-            let paddleLeft: fudge.Node = childenListLeft[0];
-            let paddleRight: fudge.Node = childenListRight[0];
-
-            switch (event.key) {
-                case "w":
-                    paddleRight.cmpTransform.local.translateY(0.1);
-                    break;
-                case "s":
-                    paddleRight.cmpTransform.local.translateY(-0.1);
-                    break;
-                case "ArrowUp":
-                    paddleLeft.cmpTransform.local.translateY(0.1);
-                    break;
-                case "ArrowDown":
-                    paddleLeft.cmpTransform.local.translateY(-0.1);
-                    break;
-            }
-            fudge.RenderManager.update();
-            viewport.draw();
-            
-            
-        })
+        addEventListener("keydown", hndlKeyDown);
+        addEventListener("keyup", hndlKeyUp);
         viewport.draw();
-        
+        fudge.Loop.addEventListener(fudge.EVENT.LOOP_FRAME, update);
+        fudge.Loop.start();
     }
 
     function createPong(): fudge.Node {
         //Nodes
         let pongNode: fudge.Node = new fudge.Node("Pong Node");
         let ball: fudge.Node = new fudge.Node("Ball");
-        let leftPaddle: fudge.Node = new fudge.Node("LeftPaddle");
-        let rightPaddle: fudge.Node = new fudge.Node("RightPaddle");
+        paddleLeft = new fudge.Node("paddleLeft");
+        paddleRight = new fudge.Node("paddleRight");
 
         // Left Paddle Compnent und Mesh und Color
         let meshQuad: fudge.MeshQuad = new fudge.MeshQuad();
@@ -69,16 +65,16 @@ namespace L03PongPaddles {
         let BallMtrComponent: fudge.ComponentMaterial = new fudge.ComponentMaterial(mtrSolidWhite);
 
         //Left Paddle Node zusamensetzten und zu Pong Node hinzufügen
-        leftPaddle.addComponent(mtrComponent);
-        leftPaddle.addComponent(cmpMesh);
-        leftPaddle.addComponent(new fudge.ComponentTransform);
-        pongNode.appendChild(leftPaddle);
+        paddleLeft.addComponent(mtrComponent);
+        paddleLeft.addComponent(cmpMesh);
+        paddleLeft.addComponent(new fudge.ComponentTransform);
+        pongNode.appendChild(paddleLeft);
 
         //Right Paddle Node zusammensetzten und zu Pong Node hinzufügen
-        rightPaddle.addComponent(quad02MtrComponent);
-        rightPaddle.addComponent(cmpQuad02);
-        rightPaddle.addComponent(new fudge.ComponentTransform);
-        pongNode.appendChild(rightPaddle);
+        paddleRight.addComponent(quad02MtrComponent);
+        paddleRight.addComponent(cmpQuad02);
+        paddleRight.addComponent(new fudge.ComponentTransform);
+        pongNode.appendChild(paddleRight);
 
         //Ball Node zusammensetzten und zu Pong Node hinzufügen
         ball.addComponent(cmpBall);
@@ -86,24 +82,41 @@ namespace L03PongPaddles {
         ball.addComponent(new fudge.ComponentTransform);
         pongNode.appendChild(ball);
 
-        //let cmpTransform: fudge.ComponentTransform = rightPaddle.getComponent(fudge.ComponentTransform) //cmpTransform geht auch
-        leftPaddle.cmpTransform.local.translateX(1.3);
-        rightPaddle.cmpTransform.local.translateX(-1.3);
+        //let cmpTransform: fudge.ComponentTransform = paddleRight.getComponent(fudge.ComponentTransform) //cmpTransform geht auch
+        paddleLeft.cmpTransform.local.translateX(1.3);
+        paddleRight.cmpTransform.local.translateX(-1.3);
 
         // TODO Nebeneffekte möglich Besser das Mesh Skalieren
         ball.cmpTransform.local.scaleY(0.2);
         ball.cmpTransform.local.scaleX(0.2);
         ball.cmpTransform.local.scaleZ(0.2);
 
-        leftPaddle.cmpTransform.local.scaleX(0.15);
-        rightPaddle.cmpTransform.local.scaleX(0.15);
+        paddleLeft.cmpTransform.local.scaleX(0.15);
+        paddleRight.cmpTransform.local.scaleX(0.15);
 
         return pongNode;
     }
 
+    function update(_event: Event): void {
+        //fudge.Debug.log(keysPressed);
+        fudge.RenderManager.update();
+        viewport.draw();
+
+        if (keysPressed["w"]) {
+            paddleRight.cmpTransform.local.translateY(0.025);
+        }
+        if (keysPressed["s"]) {
+            paddleRight.cmpTransform.local.translateY(-0.025);
+        }
+        if (keysPressed["ArrowUp"]) {
+            paddleLeft.cmpTransform.local.translateY(0.025);
+        }
+        if(keysPressed["ArrowDown"]) {
+            paddleLeft.cmpTransform.local.translateY(-0.025);
+        }
+
+    }
 }
-
-
 /*
 Pong
 Eventlistener auf die Tasten (Keyboardevent)
@@ -111,9 +124,19 @@ Objekte müssen bewegt werden
 Vorischt was man bewegen will. Die Meshes oder vielleicht doch besser die Knoten
 Backface Culling beachten
 set invervall und alle x Sekunden etwas aufrufen.
-Vorsicht mit anonymen Funktionen
+Vorsicht mit anonymen Funktionen => innerhalb von eventlistener
 
-Hausaufgaben
-Paddles mit Tasen rauf und runter bewgen.
-Knoten Transformation benuten
+Tastatureingabe
+Möglichkeit Nummer 1 Array String speichern und bei Veränderung reagieren
+Möglichkeit Nummer 2 Assoziatives Array (Map oder standart Javascript Objekte, also geschweifte Klammern)
+let keyspressed = {} Assoziatives Array
+Keydown: Wenn was kommt in das Array den Code speichern der ankomme
+Keyup: Den Code auf false setzten oder löschen
+Herausfinden, ob ein bestimmter Key gedrückt wurde
+    File Keypressed fudge keyboard code bspw. up ist true, dann darauf reagieren
+
+Aufgabe für Dienstag
+Ball bewegen, so dass er an allen vier Wänden abprall. Starten in zufällige Richtung und Geschwindigkeit
+Vector3 verwenden für die Geschwindigkeit soll am Anfang zufällig bestimmt werden
+Ränder definieren
 */
